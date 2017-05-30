18c3455bdcb571ac97a58b8d3f9ca784f0846e14
huynhhuyhoang <huyhoang.rot@gmail.com>
2017-05-31 04:32:06 +0700

    up

--- lang/zh_CN/Package/StandardPackage.lua
+++ lang/zh_CN/Package/StandardPackage.lua
@@ -1,126 +1,126 @@
 -- translation for StandardPackage
 
 local t = {
-	["standard_cards"] = "标准版",
+	["standard_cards"] = "THẺ TIÊU CHUẨN",
 
-	["slash"] = "Sát",
+	["slash"] = "SÁT",
 	[":slash"] = "Thẻ cơ bản<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：Người chơi trong tầm đánh.<br /><b>Hiệu quả</b>：Gây 1 điểm sát thương cho mục tiêu",
 	["slash-jink"] = "%src đã dùng 【Sát】, hãy dùng 1 thẻ 【Tránh】",
 	["@multi-jink-start"] = "%src đã dùng 【Sát】, hãy dùng %arg thẻ 【Tránh】",
 	["@multi-jink"] = "%src đã dùng 【Sát】, hãy dùng thêm %arg thẻ 【Tránh】】",
 	["@slash_extra_targets"] = "Hãy dùng 【Sát】 lên mục tiêu khác",
 
-	["jink"] = "Ngự",
+	["jink"] = "TRÁNH",
 	[":jink"] = "Thẻ cơ bản<br /><b>Sử dụng</b>：Có hiệu quả khi là mục tiêu của 【Sát】<br /><b>Mục tiêu</b>：Là mục tiêu của 【Sát】<br /><b>Hiệu quả</b>：Vô hiệu hoá 【Sát】。",
-	["#NoJink"] = "%from 不能使用【<font color=\"yellow\"><b>闪</b></font>】响应此【<font color=\"yellow\"><b>杀</b></font>】",
+	["#NoJink"] = "%from không thể dùng【<font color=\"yellow\"><b>Tránh</b></font>】để vô hiệu hóa【<font color=\"yellow\"><b>Sát</b></font>】",
 
-	["peach"] = "Đào",
+	["peach"] = "ĐÀO",
 	[":peach"] = "Thẻ cơ bản<br /><b>Sử dụng</b>：một lần trong lượt hành động/Khi trong trạng thái hấp hối<br /><b>Mục tiêu</b>：khi bị mất máu/khi một người chơi đang trong trạng thái hấp hối<br /><b>Hiệu quả</b>：Mục tiêu hồi 1 máu。",
 
-	["crossbow"] = "Gia cát liên nỏ",
+	["crossbow"] = "GIA CÁT LIÊN NỎ",
 	[":crossbow"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：１<br /><b>Chức năng</b>：Tọa Định Kỷ。Dùng lá 【Sát】 không hạn chế。",
 
-	["double_sword"] = "Thư hùng song cổ ki",
+	["double_sword"] = "THƯ HÙNG SONG CỔ KIẾM",
 	[":double_sword"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：２<br /><b>Chức năng</b>：Khi dùng 【Sát】 cho mục tiêu khác giới, cho mục tiêu chọn bỏ 1 thẻ bài trên tay hoặc để bạn rút 1 thẻ bài.",
 	["double-sword-card"] = "%src kích hoạt hiệu quả 【Thư Hùng Song Cổ Kiếm】, bạn cần phải bỏ 1 thẻ bài trên tay hoặc để %src rút 1 thẻ bài",
 
-	["qinggang_sword"] = "Thanh công kiếm",
+	["qinggang_sword"] = "THANH CÔNG KIẾM",
 	[":qinggang_sword"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：２<br /><b>Chức năng</b>：Toả Định Kỹ，Vô hiệu hoá phòng cụ của mục tiêu khi sử dụng 【Sát】",
 
-	["blade"] = "Thanh long yển nguyệt đao",
-	[":blade"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：３<br /><b>Chức năng</b>：每当你使用的【杀】被【闪】抵消后，你可以对该角色再使用一张【杀】（无距离限制且不能选择额外Mục tiêu）。",
-	["blade-slash"] = "你可以发动【青龙偃月刀】再对 %src 使用一张【杀】",
-	["#BladeUse"] = "%from 对 %to 发动了【<font color=\"yellow\"><b>青龙偃月刀</b></font>】Hiệu quả",
+	["blade"] = "THANH LONG YỂN NGUYỆT ĐAO",
+	[":blade"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：３<br /><b>Chức năng</b>：Mỗi khi bạn dùng SÁT và bị TRÁNH, bạn được SÁT lên người chơi khác （không giới hạn tầm và không được chọn thêm mục tiêu）。",
+	["blade-slash"] = "Bạn có thể kích hoạt 【THANH LONG YỂN NGUYỆT ĐAO】 và dùng SÁT lên %src",
+	["#BladeUse"] = "%from sử dụng 【<font color=\"yellow\"><b>THANH LONG YỂN NGUYỆT ĐAO</b></font>】 lên %to",
 
-	["spear"] = "Trượng bát xà mâu",
-	[":spear"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：３<br /><b>Chức năng</b>：你可以将两张手牌当【杀】使用或打出。",
+	["spear"] = "TRƯỢNG BÁT XÀ MÂU",
+	[":spear"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：３<br /><b>Chức năng</b>： có thể dùng 2 lá bất kỳ trên tay như 【Sát】。",
 
-	["axe"] = "Quán thạch phủ",
-	[":axe"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：３<br /><b>Chức năng</b>：每当你使用的【杀】被【闪】抵消后，你可以弃置两张牌，则此【杀】继续造成伤害。",
-	["@axe"] = "你可以弃置两张牌令此【杀】继续造成伤害",
-	["~axe"] = "选择两张牌→点击确定",
+	["axe"] = "QUÁN THẠCH PHỦ",
+	[":axe"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：３<br /><b>Chức năng</b>：Khi mục tiêu tấn công dùng 【Tránh】, có thể bỏ 2 thẻ bài để vô hiệu hoá thẻ 【Tránh】 đó。",
+	["@axe"] = "Bạn có thể bỏ ra 2 thẻ bài để 【Sát】 vẫn có hiệu lực.",
+	["~axe"] = "Chọn 2 thẻ bài muốn bỏ rồi bấm OK",
 
-	["halberd"] = "Phương thiên họa kích",
-	[":halberd"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：４<br /><b>Chức năng</b>：锁定技。你使用最后的手牌【杀】可以额外选择至多两名Mục tiêu。",
+	["halberd"] = "PHƯƠNG THIÊN HỌA KÍCH",
+	[":halberd"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：４<br /><b>Chức năng</b>：Tọa Định Kỷ。Bạn có thể dùng 1 lá SÁT lên hai mục tiêu。",
 
-	["kylin_bow"] = "Kỳ Lân Cung",
-	[":kylin_bow"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：５<br /><b>Chức năng</b>：每当你使用【杀】对Mục tiêu角色造成伤害时，你可以弃置其装备区内的一张坐骑牌。",
-	["kylin_bow:dhorse"] = "+1坐骑",
-	["kylin_bow:ohorse"] = "-1坐骑",
+	["kylin_bow"] = "KỲ LÂN CUNG",
+	[":kylin_bow"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：５<br /><b>Chức năng</b>：Khi dùng 【Sát】 gây thiệt hại cho người chơi, có thể loại bỏ một Chiến mã trên khu trang bị của mục tiêu。",
+	["kylin_bow:dhorse"] = "+1 Chiến mã",
+	["kylin_bow:ohorse"] = "-1 Chiến mã",
 
-	["eight_diagram"] = "Bát Quái Trận",
-	[":eight_diagram"] = "Thẻ trang bị - Phòng cụ<br /><b>防具技能</b>：每当你需要使用或打出一张【闪】时，你可以进行判定：若结果为红色，视为你使用或打出了一张【闪】。",
+	["eight_diagram"] = "BÁT QUÁI TRẬN",
+	[":eight_diagram"] = "Thẻ trang bị - Phòng cụ<br /><b>Chức năng</b>：Bất kỳ lúc nào cần dùng 【Tránh】, có thể rút Phán xét, nếu bài sắc Đỏ sẽ xem như TRÁNH",
 
-	["standard_ex_cards"] = "标准版EX",
+	["standard_ex_cards"] = "THẺ TIỂU CHUẨN MỞ RỘNG",
 
-	["renwang_shield"] = "Nhân Vương Thuẫn",
-	[":renwang_shield"] = "Thẻ trang bị - Phòng cụ<br /><b>防具技能</b>：锁定技。黑色【杀】对你无效。",
+	["renwang_shield"] = "NHÂN VƯƠNG THUẪN",
+	[":renwang_shield"] = "Thẻ trang bị - Phòng cụ<br /><b>Chức năng</b>：Tọa Định Kỷ. Vô hiệu hóa SÁT đen.",
 
-	["ice_sword"] = "Hàn băng kiếm",
-	[":ice_sword"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：２<br /><b>Chức năng</b>：每当你使用【杀】对Mục tiêu角色造成伤害时，若该角色有牌，你可以防止此伤害，然后依次弃置其两张牌。",
-	["ice_sword:yes"] = "你可以依次弃置其两张牌",
+	["ice_sword"] = "HÀN BĂNG KIẾM",
+	[":ice_sword"] = "Thẻ trang bị - Vũ khí<br /><b>Tầm đánh</b>：２<br /><b>Chức năng</b>： Khi gây thiệt hại bằng 【Sát】, bạn có thể chọn gây sát thương hoặc huỷ 2 thẻ bài của mục tiêu.",
+	["ice_sword:yes"] = "Bạn có thể bỏ 2 thẻ bài của mục tiêu",
 
-	["horse"] = "Tọa Kị",
-	[":+1 horse"] = "Thẻ trang bị - Tọa Kị<br /><b>坐骑技能</b>：其他角色与你的距离+1。",
-	["jueying"] = "Tuyệt Ảnh",
-	["dilu"] = "Đích Lô",
-	["zhuahuangfeidian"] = "Trảo hoàng phi điện",
-	[":-1 horse"] = "Thẻ trang bị - Tọa Kị<br /><b>坐骑技能</b>：你与其他角色的距离-1。",
-	["chitu"] = "Xích Thố",
-	["dayuan"] = "Đại Uyển",
-	["zixing"] = "Tử Tinh",
+	["horse"] = "CHIẾN MÃ",
+	[":+1 horse"] = "Thẻ trang bị - Tọa Kị<br /><b>Kỹ năng</b>：Cự ly giữa người chơi khác và bản thân +1.",
+	["jueying"] = "TUYỆT ẢNH",
+	["dilu"] = "ĐÍCH LÔ",
+	["zhuahuangfeidian"] = "TRẢO HOÀNG PHI ĐIỆN",
+	[":-1 horse"] = "Thẻ trang bị - CHIẾN MÃ<br /><b>Kỹ năng</b>Cự ly của bạn với người chơi khác -1.",
+	["chitu"] = "XÍCH THỐ",
+	["dayuan"] = "ĐẠI UYỂN",
+	["zixing"] = "TỬ TINH",
 
-	["amazing_grace"] = "Ngũ Cốc Phong Đăng",
-	[":amazing_grace"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：所有角色<br /><b>Hiệu quả</b>：你亮出牌堆顶等于角色数的牌，每名Mục tiêu角色获得其中一张牌，然后将其余的牌置入弃牌堆。",
+	["amazing_grace"] = "NGŨ CỐC PHONG ĐĂNG",
+	[":amazing_grace"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：Tất cả người chơi<br /><b>Hiệu quả</b>： Lật ra từ tập bài số thẻ bằng với số người chơi hiện tại. Mỗi người chơi lần lượt được rút 1 lá từ số thẻ này.",
 
-	["god_salvation"] = "Đào Viên Kết Nghĩa",
-	[":god_salvation"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：所有角色<br /><b>Hiệu quả</b>：每名Mục tiêu角色回复1点体力。",
+	["god_salvation"] = "ĐÀO VIÊN KẾT NGHĨA",
+	[":god_salvation"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：Tất cả người chơi<br /><b>Hiệu quả</b>：Mỗi người chơi được hồi 1 máu",
 
-	["savage_assault"] = "Nam Man Nhập Xâm",
-	[":savage_assault"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：所有其他角色<br /><b>Hiệu quả</b>：每名Mục tiêu角色须打出一张【杀】，否则受到1点伤害。",
-	["savage-assault-slash"] = "%src 使用了【南蛮入侵】，请打出一张【杀】来响应",
+	["savage_assault"] = "NAM MAN NHẬP XÂM",
+	[":savage_assault"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：Tất cả người chơi khác<br /><b>Hiệu quả</b>：Tất cả người chơi khác xuất ra thẻ 【Sát】, nếu không nhận 1 điểm sát thương.",
+	["savage-assault-slash"] = "%src đã kích phát 【Nam Man Xâm Lược】, hãy xuất ra thẻ 【Sát】",
 
-	["archery_attack"] = "Vạn Tiễn Tề Phát",
-	[":archery_attack"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：所有其他角色<br /><b>Hiệu quả</b>：每名Mục tiêu角色须打出一张【闪】，否则受到1点伤害。",
-	["archery-attack-jink"] = "%src 使用了【万箭齐发】，请打出一张【闪】以响应",
+	["archery_attack"] = "VẠN TIỄN TỀ PHÁT",
+	[":archery_attack"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：Tất cả người chơi khác<br /><b>Hiệu quả</b>：Tất cả người chơi còn lại xuất ra thẻ 【Tránh】, nếu không nhận 1 điểm sát thương.",
+	["archery-attack-jink"] = "%src đã kích phát 【Vạn Tiễn Tề Phát】, hãy xuất ra thẻ 【Tránh】",
 
-	["collateral"] = "Tá Đao Sát Nhân",
-	[":collateral"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：装备区内有武器牌且攻击范围内有【杀】的合法Mục tiêu的一名其他角色A（你需要选择一名A攻击范围内的【杀】的合法Mục tiêuB）<br /><b>Hiệu quả</b>：A须对B使用一张【杀】，否则你获得A装备区内的武器牌。",
-	["collateral-slash"] = "%dest 使用了【借刀杀人】，请对 %src 使用一张【杀】",
-	["#CollateralSlash"] = "%from 选择了此【<font color=\"yellow\"><b>杀</b></font>】的Mục tiêu %to",
+	["collateral"] = "TÁ ĐAO SÁT NHÂN",
+	[":collateral"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：Một người chơi đang trang bị vũ khí và một mục tiêu khác trong tầm có thể dùng 【Sát】.<br /><b>Hiệu quả</b>：Buộc người chơi dùng 【Sát】 lên mục tiêu kia，Nếu không dùng 【Sát】 lên mục tiêu chỉ định, bạn có thể lấy vũ khí của người đó.",
+	["collateral-slash"] = "%dest đã kích phát 【Tá Đao Sát Nhân】, yêu cầu dùng 【Sát】 lên %src】",
+	["#CollateralSlash"] = "%from đã chọn dùng 【<font color=\"yellow\"><b>Sát</b></font>】 lên %to",
 
 	["duel"] = "QUYẾT ĐẤU",--决斗
-	[":duel"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：一名其他角色<br /><b>Hiệu quả</b>：由Mục tiêu角色开始，你与其轮流：打出一张【杀】，否则受到对方的1点伤害并结束此牌结算。",
-	["duel-slash"] = "%src 对你【决斗】，你需要打出一张【杀】",
+	[":duel"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：Một người chơi khác.<br /><b>Hiệu quả</b>：Bắt đầu từ mục tiêu và bạn lần lượt xuất ra thẻ 【Sát】 đến khi có người không thể tiếp tục dùng 【Sát】 sẽ nhận 1 điểm sát thương.",
+	["duel-slash"] = "%src phát động 【Quyết Đấu】 với bạn, hãy xuất ra 1 thẻ 【Sát】",
 
 	["ex_nihilo"] = "VÔ TRUNG SINH HỮU",--无中生有
-	[":ex_nihilo"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：你<br /><b>Hiệu quả</b>：Mục tiêu角色摸两张牌。",
+	[":ex_nihilo"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：Bản thân.<br /><b>Hiệu quả</b>: Rút 2 thẻ bài.",
 
-	["snatch"] = "THUẬN THỦ kHIÊN DƯƠNG",--顺手牵羊
-	[":snatch"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：距离1的一名区域内有牌的角色<br /><b>Hiệu quả</b>：你获得Mục tiêu角色区域内的一张牌。",
+	["snatch"] = "THUẬN THỦ KHIÊN DƯƠNG",--顺手牵羊
+	[":snatch"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：Một người chơi khác có thẻ bài trong tầm 1 của bạn.<br /><b>Hiệu quả</b>： Rút về 1 thẻ bài của mục tiêu.",
 
-	["dismantlement"] = "过河拆桥",
-	[":dismantlement"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：一名区域内有牌的其他角色。<br /><b>Hiệu quả</b>：你弃置Mục tiêu角色区域内的一张牌。",
+	["dismantlement"] = "QUÁ HÀ SÁCH KIỀU",
+	[":dismantlement"] = "Thẻ công cụ<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：Một người chơi khác có thẻ bài.<br /><b>Hiệu quả</b>：Huỷ bỏ 1 lá bài của mục tiêu.",
 
-	["nullification"] = "Vô Giải Khả Kích", --无懈可击
-	[":nullification"] = "Thẻ công cụ<br /><b>Sử dụng</b>：Thẻ công cụ对Mục tiêu角色生效前，或一张【无懈可击】生效前<br /><b>Mục tiêu</b>：该Thẻ công cụ<br /><b>Hiệu quả</b>：抵消该Thẻ công cụ对该角色产生的Hiệu quả，或抵消另一张【无懈可击】产生的Hiệu quả。",
+	["nullification"] = "VÔ GIẢI KHẢ KÍCH", --无懈可击
+	[":nullification"] = "Thẻ công cụ<br /><b>Sử dụng</b>：Khi có thẻ công cụ tác động lên một người chơi.<br /><b>Mục tiêu</b>：Thẻ công cụ<br /><b>Hiệu quả</b>： Vô hiệu hóa hiệu quả của thẻ công cụ đó trên người chơi đó, hoặc vô hiệu hóa một thẻ VÔ GIẢI KHẢ KÍCH khác.",
 
-	["indulgence"] = "Lạc Bất Tư Thục",--乐不思蜀
-	[":indulgence"] = "Thẻ công cụ duy trì<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：一名其他角色<br /><b>Hiệu quả</b>：将此牌置于Mục tiêu角色判定区内。其判定阶段进行判定：若结果不为红桃，其跳过một lần trong lượt hành động。然后将【乐不思蜀】置入弃牌堆。",
+	["indulgence"] = "LẠC BẤT TƯ THỤC",--乐不思蜀
+	[":indulgence"] = "Thẻ công cụ duy trì<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：Một người chơi khác<br /><b>Hiệu quả</b>： Người chơi mục tiêu lật phát xét, nếu không phải Cơ sẽ mất lượt hành động.",
 
 	["lightning"] = "THIỂM ĐIỆN",--闪电
-	[":lightning"] = "Thẻ công cụ duy trì<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：你<br /><b>Hiệu quả</b>：将此牌置于Mục tiêu角色判定区内。其判定阶段进行判定：若结果为黑桃2-9，其受到3点雷电伤害并将【闪电】置入弃牌堆，否则将【闪电】移动至其下家判定区内。",
+	[":lightning"] = "Thẻ công cụ duy trì<br /><b>Sử dụng</b>：một lần trong lượt hành động<br /><b>Mục tiêu</b>：Bản thân<br /><b>Mục tiêu rút phán xét, nếu từ 2~9 Bích sẽ nhận 3 điểm sát thương Lôi hệ (không nguồn) và loại bỏ thẻ này. Nếu không, thẻ 【Thiểm Điện】 chuyển sang mục tiêu bên cạnh.",
 
 
-	["limitation_broken"] = "界限突破卡牌",
+	["limitation_broken"] = "THẺ ĐỘT PHÁ",
 
-	["wooden_ox"] = "Mộc Ngưu Lưu Mã",
-	[":wooden_ox"] = "装备牌·宝物<br /><b>宝物技能</b>：<br />" ..
-					"1. 阶段技。你可以将一张手牌置于【木牛流马】下：若如此做，你可以将【木牛流马】移动至一名其他角色的装备区。<br />" ..
-					"2. 你可以将【木牛流马】下的牌视为手牌使用或打出。<br />" ..
-					"◆每当你失去装备区的【木牛流马】后，若【木牛流马】未移动至装备区，其下的牌置入弃牌堆，否则这些牌仍置于【木牛流马】下。<br />◆【木牛流马】下的牌为移出游戏。",
-	["@wooden_ox-move"] = "你可以将【木牛流马】移动至一名其他角色的装备区",
-	["#WoodenOx"] = "%from 使用/打出了 %arg 张 %arg2 牌",
+	["wooden_ox"] = "MỘC NGƯU LƯU MÃ",
+	[":wooden_ox"] = "Thẻ Trang bị - Bảo vật<br /><b>Chức năng</b>：<br />" ..
+					"1. Một lần trong lượt hành động, có thể bỏ 1 thẻ trên tay vào dưới 【Mộc Ngưu Lưu Mã】, nếu vậy bạn có thể chuyển 【Mộc Ngưu Lưu Mã】 sang khu trang bị của người chơi khác.<br />" ..
+					"2. Có thể sử dụng thẻ bài dưới 【Mộc Ngưu Lưu Mã】 như bài trên tay.<br />" ..
+					"◆Mỗi khi bạn dùng hoặc mất 【Mộc Ngưu Lưu Mã】, nếu thẻ này không chuyển sang khu trang bị khác, thì thẻ bài bên dưới nó sẽ bị huỷ, không thì vẫn nằm trong 【Mộc Ngưu Lưu Mã】.<br />◆Thẻ bên dưới 【Mộc Ngưu Lưu Mã】 bị loại khỏi trận đấu.",
+	["@wooden_ox-move"] = "Bạn có thể chuyển 【Mộc Ngưu Lưu Mã】 sang khu trang bị của một người chơi khác",
+	["#WoodenOx"] = "%from xuất ra %arg và %arg2",
 }
 
 local ohorses = { "chitu", "dayuan", "zixing" }
